39eebae8c7c32e7a8ad63f14ca0edaf2b533fbc7
Enzo949494 <9812@holbertonstudents.com>
2024-12-10 14:48:11 +0100

    finds the uncle of a node,fix problem check return

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,17 +9,16 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	{
-	binary_tree_t *grandparent = node->parent->parent;
-
-	if (node == NULL || node->parent == NULL)
+	if (!node)
 		return (NULL);
 
-
-	if (grandparent == NULL)
-		return (NULL);
-
-	if (node->parent == grandparent->left)
-		return (grandparent->right);
-
-	return (grandparent->left);
+	if (node->parent && node->parent->parent)
+	{
+		if (node->parent->parent->left == node->parent)
+			return (node->parent->parent->right);
+		if (node->parent->parent->right == node->parent)
+			return (node->parent->parent->left);
 	}
+	return (NULL);
+	}
+
