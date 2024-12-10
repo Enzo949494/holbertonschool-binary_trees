598753ae9d26e6cd002f3fb93fbc32feb7d67bc2
Enzo949494 <9812@holbertonstudents.com>
2024-12-10 13:06:44 +0100

    measures the balance factor of a binary tree, fix probleme declarations arguments

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -30,8 +30,10 @@
 
 	int binary_tree_balance(const binary_tree_t *tree)
 	{
-	int left_height = (int)binary_tree_height(tree->left);
-	int right_height = (int)binary_tree_height(tree->right);
+	int left_height, right_height;
+
+	left_height = (int)binary_tree_height(tree->left);
+	right_height = (int)binary_tree_height(tree->right);
 
 	if (tree == NULL)
 		return (0);
